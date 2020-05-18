c5744740a8cc2a96c16a7afda7bcd39e3f6da3e2
Alyssa Schmalenberger <aschmalenberger@gmail.com>
2020-05-18 13:49:52 -0500

    adding omdb api info

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-
-//Marvel API Call
-// var searchInput = "spider-man";
-// var APIKey = "c67e2837f6363498c7e1e28ebcf18af5";
+var searchInput = "spider-man";
+var APIKey = "c67e2837f6363498c7e1e28ebcf18af5";
+var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchInput + "&apikey=" + APIKey;
   
-//function characterDisplay (searchInput) {
-    //var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchInput + "&apikey=" + APIKey;
-    //var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=spider-man&apikey=c67e2837f6363498c7e1e28ebcf18af5";
+function characterDisplay (searchInput) {
 
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
     $.ajax({
-      url: "https://gateway.marvel.com:443/v1/public/characters?name=spider-man&apikey=c67e2837f6363498c7e1e28ebcf18af5",
-      
+      url: queryURL,
       method: "GET"
     }).then(function(response) {
 
@@ -18,5 +14,13 @@
       console.log(response);
 
     });
-//}
-//characterDisplay(searchInput);
+}
+
+$.ajax({
+  url: omdbQueryURL,
+  method: "GET"
+}).then(function(response) {
+
+  // Printing the entire object to console
+  console.log(response);
+})
