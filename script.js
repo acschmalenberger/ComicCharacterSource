bf0a2b5a2455a7c829af7695c0192002af59f561
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-18 13:40:31 -0500

    Added jquery to index and started working with script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-var searchInput = "spider-man";
-var APIKey = "c67e2837f6363498c7e1e28ebcf18af5";
-var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchInput + "&apikey=" + APIKey;
+
+//Marvel API Call
+// var searchInput = "spider-man";
+// var APIKey = "c67e2837f6363498c7e1e28ebcf18af5";
   
-function characterDisplay (searchInput) {
+//function characterDisplay (searchInput) {
+    //var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchInput + "&apikey=" + APIKey;
+    //var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=spider-man&apikey=c67e2837f6363498c7e1e28ebcf18af5";
 
     // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
     $.ajax({
-      url: queryURL,
+      url: "https://gateway.marvel.com:443/v1/public/characters?name=spider-man&apikey=c67e2837f6363498c7e1e28ebcf18af5",
+      
       method: "GET"
     }).then(function(response) {
 
@@ -14,4 +18,5 @@ function characterDisplay (searchInput) {
       console.log(response);
 
     });
-}
+//}
+//characterDisplay(searchInput);
