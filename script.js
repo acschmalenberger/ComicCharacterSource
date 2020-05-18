fab80834a91ee28c1d7ca0364b8b13a81ddbaf7b
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-18 13:28:26 -0500

    started wiork on Marvel API call. NOthing working

---
+++ script.js
@@ -0,0 +1,17 @@
+var searchInput = "spider-man";
+var APIKey = "c67e2837f6363498c7e1e28ebcf18af5";
+var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=" + searchInput + "&apikey=" + APIKey;
+  
+function characterDisplay (searchInput) {
+
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      // Printing the entire object to console
+      console.log(response);
+
+    });
+}
