e467516d6a6b5f7b8fbf32edcc6f39de862d2884
Alyssa Schmalenberger <aschmalenberger@gmail.com>
2020-05-19 11:30:25 -0500

    finally got omdb to start working

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 var apiKey = "10206952649931006";
 var baseUrl = "https://superheroapi.com/api.php/";
 var characterName = "thor";
+var omdbAPIKey= "3e9920ff";
+var omdbQueryURL="https://www.omdbapi.com/?s=";
+var omdbIMGQueryURL: "http://img.omdbapi.com/?s="
 
+//http://img.omdbapi.com/?s=thor&apikey=3e9920ff
 //"https://superheroapi.com/api/10206952649931006/search/thor"
 
-
+console.log(omdbQueryURL + characterName + "&apikey="+ apiKey);
 function userInput () {
 
-$.ajax({
-  url: baseUrl + apiKey + "/search/" + characterName,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-}
+  $.ajax({
+    url: baseUrl + apiKey + "/search/" + characterName,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
 
+  $.ajax({
+    url: omdbQueryURL + characterName + "&apikey="+ omdbAPIKey,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+}
 userInput();
+
+
+
+
+
+
