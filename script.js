5aa3d1bb00e4b5a0e428d662fb42455c2fb8a5f1
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-05-20 11:26:56 -0500

    both API calls are working. Need styling and some dynamic creattion elms

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var apiKey = "10206952649931006";
 var baseUrl = "https://superheroapi.com/api.php/";
 var omdbAPIKey= "3e9920ff";
 var omdbQueryURL="https://www.omdbapi.com/?s=";
-
+//var omdbIMGQueryURL: "http://img.omdbapi.com/?s="
 
 
 //https://www.omdbapi.com/?s=thor&apikey=3e9920ff
@@ -17,12 +17,6 @@ $.ajax({
   //checking to see if the API call is working
   console.log(searchResponse);
   //Dynamically creating a div to house the search results title
-    
-  
-  
-  
-  $("#body").empty();
-    var searchResultsOutputText = $("<h2>").text("Halt mortal, did you mean:");
     $("#body").empty();
     var searchResultsOutputText = $("<h2>").text("Halt mortal, select the character you meant:");
     $("#body").append(searchResultsOutputText);
@@ -42,14 +36,14 @@ $.ajax({
         //adding the img to the search array div
         searchResultDiv.append(searchResultImg);
         $(".row"). append(searchResultDiv);
-        //test console log to CYA
+//test console log to CYA
         console.log(searchResponse.results[i].name);
       }
-
 
       $("img").click(function (event) {
         event.preventDefault ();
         console.log($(this).attr("data-name"));
+           
         
         userChoice($(this).attr("data-name"));
       
@@ -66,6 +60,11 @@ $.ajax({
 // var charStats = $("<div>").attr("class", col);
 // row.apped(charInfoDiv, charImg,charStats);
 
+
+
+
+
+
 function userChoice (name) {
 // var name = $(this).attr("data-name");
 console.log(name);
@@ -80,78 +79,31 @@ console.log(name);
   //character picked on previous screen will be search for OMDB, activated by click
    for (var j = 0; j < clickResponse.Search.length; j++) {
         
-        console.log(clickResponse.Search[j]);
-      //   //set up variables
+        console.log(clickResponse.Search[j])
+      //set up variables
         var movieImgUrl = clickResponse.Search[j].Poster;
         var movieResultsIMG = $("<img>").attr("src", movieImgUrl);
         
          movieResultsIMG.attr("alt", clickResponse.Search[j].Title);
         
-         $("#body").append(movieResultsIMG);
-
+         $("#body").append(movieResultsIMG);}
+})};
           
    
-     console.log(movieResultsDiv)
-    //other variables that will be used and appended to movie object
-    var movieTitle = clickResponse.Search[j].Title
+     // console.log(movieResultsDiv)
+  //   //other variables that will be used and appended to movie object
+  //   var movieTitle = clickResponse.Search[j].Title
 
-    var movieRelease = clickResponse.Search[j].Year
-  }
-};
-
-function userChoice (){
-$.ajax({
-  url: omdbQueryURL + characterName + "&apikey="+ omdbAPIKey,
-  method: "GET"
-}).then(function(clickResponse) {
-  console.log(clickResponse);
-  //$("#body").empty();
-  
-  var row2=$("<div>").attr("class","row");
-  $("#body").append(row2);
-
-  //clickResponse = //will be its own function
-  //character picked on previous screen will be search for OMDB, activated by click
-  for (var j = 0; j < clickResponse.Search.length; j++){
-    console.log(clickResponse.Search[j])
-    //create div for array
-    var clickResultDiv= $("<div>").attr("id","userChoice").attr("class", "col s4");
-
-    //create card for each item in array
-    // .attr("class", "card")
-    // var divCard = ("<div>"); 
-      //create movie image and append to card
-      var movieImgUrl = clickResponse.Search[j].Poster;
-      console.log(movieImgUrl);
-      var movieResultsImg= $("<img>").attr("src", movieImgUrl).attr("style", "width: 100%").attr("data-name",clickResponse.Search[j].Title).attr("alt", clickResponse.Search[j].Title + "image").attr("class", "card-image");
-      
-      
-      // //create movie title and append to card
-      // var movieTitle = (clickResponse.Search[j].Title).attr("class", "card-content");
-
-      
-      // //create movie release and append to card
-      // var movieRelease = (clickResponse.Search[j].Year).attr("class", "card-content");
-
-    //attach cards to column and column to row
-    // "Title: "+movieTitle, "Year: "+movieRelease);
-    // (divCard).append();
-    (clickResultDiv).append(movieResultsImg); 
-    (row2).append(clickResultDiv);
-    
-  
-  }
+  //   var movieRelease = clickResponse.Search[j].Year
+  // }
 
 
-});
-}
-// userInput();
 
-userChoice();
+//userInput();
 
 //This is to retrieve data for the search bar on the index page. 
 
-$("#comics").click(function (event) {
+$("#scour").click(function (event) {
   //prevents the page from refreshikng when a button is clicked  
   event.preventDefault();
   console.log("click");
@@ -165,8 +117,3 @@ $("#comics").click(function (event) {
   //after the data from the form is transfered into a usable object, the data is passed to the ajax call. 
   userInput(characterName);
   });
-
-
-
-
-
